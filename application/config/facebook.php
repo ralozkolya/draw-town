7efa5cd7632c47a309160ec913fa6f3df5e58e41
Nikoloz Razmadze <raloz1@gmail.com>
2017-01-25 08:20:26 +0000

    Update facebook.php

--- application/config/facebook.php
+++ application/config/facebook.php
@@ -1,6 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$config['facebook']['app_id'] = '<APP-ID>';
-$config['facebook']['app_secret'] = '<APP-SECRET>';
-$config['facebook']['default_graph_version'] = 'v2.5';
+$config['facebook']['app_id'] = get_env('FB_APP_ID');
+$config['facebook']['app_secret'] = get_env('FB_APP_SECRET');
+$config['facebook']['default_graph_version'] = get_env('FB_API_V');
